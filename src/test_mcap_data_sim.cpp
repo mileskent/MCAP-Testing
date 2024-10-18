ff385f9fe2cc1313b919341cf9b0a210c7ad288f
Miles Kent <mileskent13@gmail.com>
2024-10-18 11:47:05 -0400

    Fri Oct 18 11:47:05 EDT 2024

--- src/test_mcap_data_sim.cpp
+++ src/test_mcap_data_sim.cpp
@@ -14,21 +14,16 @@
 namespace gp = google::protobuf;
 
 // Loads the FileDescriptorSet from a protobuf schema definition into a SimpleDescriptorDatabase.
-bool LoadSchema(const mcap::SchemaPtr schema, gp::SimpleDescriptorDatabase *protoDb)
-{
+bool LoadSchema(const mcap::SchemaPtr schema, gp::SimpleDescriptorDatabase *protoDb) {
     gp::FileDescriptorSet fdSet;
-    if (!fdSet.ParseFromArray(schema->data.data(), static_cast<int>(schema->data.size())))
-    {
+    if (!fdSet.ParseFromArray(schema->data.data(), static_cast<int>(schema->data.size()))) {
         return false;
     }
     gp::FileDescriptorProto unused;
-    for (int i = 0; i < fdSet.file_size(); ++i)
-    {
+    for (int i = 0; i < fdSet.file_size(); ++i) {
         const auto &file = fdSet.file(i);
-        if (!protoDb->FindFileByName(file.name(), &unused))
-        {
-            if (!protoDb->Add(file))
-            {
+        if (!protoDb->FindFileByName(file.name(), &unused)) {
+            if (!protoDb->Add(file)) {
                 return false;
             }
         }
@@ -36,17 +31,22 @@ bool LoadSchema(const mcap::SchemaPtr schema, gp::SimpleDescriptorDatabase *prot
     return true;
 }
 
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
-        std::cerr << "Usage: " << argv[0] << " <input.mcap>" << std::endl;
-        return 1;
-    }
-    const char *inputFilename = argv[1];
+int main(int argc, char **argv) {
 
-    mcap::McapReader reader;
-    {
+    const char *inputFilename;
+    if (argc != 2) {
+        // TODO: TEMP HAVE A DEFAULT ONE FOR TESTING
+        // Assume that we are running from root dir
+
+        // std::cerr << "Usage: " << argv[0] << " <input.mcap>" << std::endl;
+        // return 1;
+
+        inputFilename = "testdata.mcap";
+    } else {
+        inputFilename = argv[1];
+    }
+
+    mcap::McapReader reader; {
         const auto res = reader.open(inputFilename);
         if (!res.ok())
         {
@@ -59,15 +59,12 @@ int main(int argc, char **argv)
     gp::DescriptorPool protoPool(&protoDb);
     gp::DynamicMessageFactory protoFactory(&protoPool);
 
-    for (auto it = messageView.begin(); it != messageView.end(); it++)
-    {
+    for (auto it = messageView.begin(); it != messageView.end(); it++) {
         // skip any non-protobuf-encoded messages.
-        if (it->schema->encoding != "protobuf")
-        {
+        if (it->schema->encoding != "protobuf") {
             continue;
         }
-        if (it->channel->messageEncoding != "protobuf")
-        {
+        if (it->channel->messageEncoding != "protobuf") {
             std::cerr << "expected channel with ID " << it->channel->id << " and schema ID "
                       << it->schema->id << "to use message encoding 'protobuf', but message encoding is "
                       << it->channel->messageEncoding << std::endl;
@@ -76,24 +73,20 @@ int main(int argc, char **argv)
         }
         const gp::Descriptor *descriptor = protoPool.FindMessageTypeByName(it->schema->name);
         // If the proto descriptor is not yet loaded, load it.
-        if (descriptor == nullptr)
-        {
-            if (!LoadSchema(it->schema, &protoDb))
-            {
+        if (descriptor == nullptr) {
+            if (!LoadSchema(it->schema, &protoDb)) {
                 reader.close();
                 return 1;
             }
             descriptor = protoPool.FindMessageTypeByName(it->schema->name);
-            if (descriptor == nullptr)
-            {
+            if (descriptor == nullptr) {
                 std::cerr << "failed to find descriptor after loading pool" << std::endl;
                 reader.close();
                 return 1;
             }
         }
         auto message = std::unique_ptr<gp::Message>(protoFactory.GetPrototype(descriptor)->New());
-        if (!message->ParseFromArray(it->message.data, static_cast<int>(it->message.dataSize)))
-        {
+        if (!message->ParseFromArray(it->message.data, static_cast<int>(it->message.dataSize))) {
             std::cerr << "failed to parse message using included foxglove.PointCloud schema" << std::endl;
             reader.close();
             return 1;
@@ -105,8 +98,8 @@ int main(int argc, char **argv)
             "\nMessage schema name: " << it->schema->name << 
             "\nMessage Log Time: " << it->message.logTime << std::endl;
         std::cout << "FIELD NAMES\n";
-        for (const auto field : fields)
-        {
+
+        for (const auto field : fields) {
             std::cout << field->name() << "\n";
         }
         break;
